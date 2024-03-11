f61132d7c2721efd30ca41fcd6436aa8683828fe
Dmitry Krivoruchko <krivoruchko.di@phystech.edu>
2024-03-11 16:38:21 +0300

    delete useless methods from class

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ class Window(QtWidgets.QMainWindow):
                           self.ui.pushButton_6, self.ui.pushButton_7, self.ui.pushButton_8,
                           self.ui.pushButton_9]
 
+        # коннект кнопок с цифрами к соотв. функциям
         for i in range(10):
             self.connect_digit_button(digits_buttons[i], i)
 
@@ -37,22 +38,34 @@ class Window(QtWidgets.QMainWindow):
         bin_opered_buttons = [self.ui.pushButton_div, self.ui.pushButton_minus,
                               self.ui.pushButton_mul, self.ui.pushButton_percent, self.ui.pushButton_plus]
 
+        # коннект кнопок, отвечающих за бинарные операции к соотв. функциям
         for i in range(5):
             self.connect_bin_oper_button(
                 bin_opered_buttons[i], [" / ", " - ", " * ", " % ", " + "][i])
 
+        # коннект кнопок, отвечающих за унарные операции к соотв. функциям
+        self.ui.pushButton_neg.clicked.connect(
+            lambda: opers.unary.turn_to_neg(self))
+        self.ui.pushButton_rev.clicked.connect(
+            lambda: opers.unary.turn_to_rev(self))
+        self.ui.pushButton_sqr.clicked.connect(
+            lambda: opers.unary.take_square(self))
+        self.ui.pushButton_sqrt.clicked.connect(
+            lambda: opers.unary.take_square_root(self))
+
+        # коннект кнопок, отвечающих за чистку к соотв. функциям
+        self.ui.pushButton_C.clicked.connect(
+            lambda: opers.clearing.clear(self))
+        self.ui.pushButton_CE.clicked.connect(
+            lambda: opers.clearing.clear_eval(self))
+        self.ui.pushButton_del.clicked.connect(
+            lambda: opers.clearing.delete(self))
+
         # коннект остальных кнопок к соотв. функциям
-        self.ui.pushButton_eq.clicked.connect(self.equalize)
-        self.ui.pushButton_dot.clicked.connect(self.make_dot)
-
-        self.ui.pushButton_C.clicked.connect(self.clear)
-        self.ui.pushButton_CE.clicked.connect(self.clear_eval)
-        self.ui.pushButton_del.clicked.connect(self.delete)
-
-        self.ui.pushButton_neg.clicked.connect(self.turn_to_neg)
-        self.ui.pushButton_rev.clicked.connect(self.turn_to_rev)
-        self.ui.pushButton_sqr.clicked.connect(self.take_square)
-        self.ui.pushButton_sqrt.clicked.connect(self.take_square_root)
+        self.ui.pushButton_eq.clicked.connect(
+            lambda: opers.other.equalize(self))
+        self.ui.pushButton_dot.clicked.connect(
+            lambda: opers.other.make_dot(self))
 
     def connect_digit_button(self, button: QtWidgets.QPushButton, number: int):
         """
@@ -64,7 +77,7 @@ class Window(QtWidgets.QMainWindow):
             number (int): цифра
         """
 
-        button.clicked.connect(lambda: self.change_number(number))
+        button.clicked.connect(lambda: opers.other.change_number(self, number))
 
     def connect_bin_oper_button(self, button: QtWidgets.QPushButton, oper: str):
         """
@@ -76,40 +89,7 @@ class Window(QtWidgets.QMainWindow):
             oper (str): операция
         """
 
-        button.clicked.connect(lambda: self.do_bin_oper(oper))
-
-    def change_number(self, number: int):
-        opers.other.change_number(self, number)
-
-    def equalize(self):
-        opers.other.equalize(self)
-
-    def make_dot(self):
-        opers.other.make_dot(self)
-
-    def do_bin_oper(self, oper: str):
-        opers.binary.do_bin_oper(self, oper)
-
-    def turn_to_neg(self):
-        opers.unary.turn_to_neg(self)
-
-    def turn_to_rev(self):
-        opers.unary.turn_to_rev(self)
-
-    def take_square(self):
-        opers.unary.take_square(self)
-
-    def take_square_root(self):
-        opers.unary.take_square_root(self)
-
-    def clear(self):
-        opers.clearing.clear(self)
-
-    def clear_eval(self):
-        opers.clearing.clear_eval(self)
-
-    def delete(self):
-        opers.clearing.delete(self)
+        button.clicked.connect(lambda: opers.binary.do_bin_oper(self, oper))
 
 
 if __name__ == "__main__":
