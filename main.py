523575a640376654ca79be508df85fe5a0fdc478
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-03-11 16:25:25 +0300

    add float numbers using

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PyQt6.QtWidgets import QWidget, QPushButton
 from PyQt6.QtCore import QSize, Qt
 from custom_button import CalcButton
 from calc import Ui_Window
-from utils import Stack
+from utils import Stack, is_float, sci_round
 
 
 class Window(QtWidgets.QMainWindow):
@@ -30,7 +30,7 @@ class Window(QtWidgets.QMainWindow):
                  self.ui.pushButton_mul, self.ui.pushButton_percent, self.ui.pushButton_plus]
 
         for i in range(5):
-            self.connect_opered_button(opers[i], [" / ", " - ", " * ", " % ", " + "][i])
+            self.connect_bin_opered_button(opers[i], [" / ", " - ", " * ", " % ", " + "][i])
 
         self.ui.pushButton_eq.clicked.connect(self.realize_equal)
 
@@ -38,28 +38,30 @@ class Window(QtWidgets.QMainWindow):
         self.ui.pushButton_CE.clicked.connect(self.realize_clear_eval)
         self.ui.pushButton_del.clicked.connect(self.realize_del)
 
+        self.ui.pushButton_dot.clicked.connect(self.realize_dot)
+
     def connect_numbed_button(self, button: QPushButton, number: int):
         button.clicked.connect(lambda: self.change_number(number))
 
-    def connect_opered_button(self, button: QPushButton, oper: str):
-        button.clicked.connect(lambda: self.realize_oper(oper))
+    def connect_bin_opered_button(self, button: QPushButton, oper: str):
+        button.clicked.connect(lambda: self.realize_bin_oper(oper))
 
     def change_number(self, number):
         print(self.calc_stack.stack)
 
-        if (self.calc_stack[-1].isdigit() and self.calc_stack[-1] != "0"):
+        if (is_float(self.calc_stack[-1]) and self.calc_stack[-1] != "0"):
             self.calc_stack[-1] = self.calc_stack[-1] + str(number)
         elif self.calc_stack[-1] == "0":
             self.calc_stack[-1] = str(number)
         else:
             self.calc_stack.push(str(number))
 
-        self.ui.labelEnter.setText(self.calc_stack[-1])
+        self.ui.labelEnter.setText(sci_round(self.calc_stack[-1]))
 
-    def realize_oper(self, oper):
+    def realize_bin_oper(self, oper):
         print(self.calc_stack.stack)
 
-        if (self.calc_stack[-1].isdigit()):
+        if (is_float(self.calc_stack[-1])):
             if (self.calc_stack.size() > 2):
                 evaluation: list = [self.calc_stack.pop()]
                 evaluation.append(self.calc_stack.pop())
@@ -67,30 +69,35 @@ class Window(QtWidgets.QMainWindow):
                 # print("".join(reversed(evaluation)))
                 self.calc_stack.push(str(eval("".join(reversed(evaluation)))))
                 # print(str(eval("".join(reversed(evaluation)))))
-                self.ui.labelEnteredExpression.setText(self.calc_stack[-1] + oper)
-                self.ui.labelEnter.setText(self.calc_stack[-1])
+                self.ui.labelEnteredExpression.setText(sci_round(self.calc_stack[-1]) + oper)
+                self.ui.labelEnter.setText(sci_round(self.calc_stack[-1]))
             else:
-                self.ui.labelEnteredExpression.setText(self.calc_stack[-1] + oper)
+                self.ui.labelEnteredExpression.setText(sci_round(self.calc_stack[-1]) + oper)
             self.calc_stack.push(oper)
 
         else:
-            prev_oper = self.calc_stack.pop()
+            self.calc_stack.pop()
             number: str = self.calc_stack[-1]
             self.calc_stack.push(oper)
-            self.ui.labelEnteredExpression.setText(number + oper)
+
+            self.ui.labelEnteredExpression.setText(sci_round(number) + oper)
 
     def realize_equal(self):
         print(self.calc_stack.stack)
 
-        if (self.calc_stack[-1].isdigit() and self.calc_stack.size() > 1):
+        if (is_float(self.calc_stack[-1]) and self.calc_stack.size() > 1):
             evaluation: list = [self.calc_stack.pop()]
             evaluation.append(self.calc_stack.pop())
             evaluation.append(self.calc_stack.pop())
-            # print("".join(reversed(evaluation)))
-            self.calc_stack.push(str(eval("".join(reversed(evaluation)))))
-            # print(str(eval("".join(reversed(evaluation)))))
-            self.ui.labelEnteredExpression.setText("".join(reversed(evaluation)) + " =")
-            self.ui.labelEnter.setText(self.calc_stack[-1])
+
+            try:
+                self.calc_stack.push(str(eval("".join(reversed(evaluation)))))
+                self.ui.labelEnteredExpression.setText(
+                    sci_round("".join(reversed(evaluation))) + " =")
+                self.ui.labelEnter.setText(sci_round(self.calc_stack[-1]))
+            except ZeroDivisionError:
+                self.ui.labelEnter.setText("Zero division")
+
         elif (self.calc_stack.size() > 1):
             evaluation: list = [self.calc_stack.pop()]
             number: str = self.calc_stack.pop()
@@ -99,10 +106,10 @@ class Window(QtWidgets.QMainWindow):
             # print("".join(reversed(evaluation)))
             self.calc_stack.push(str(eval("".join(reversed(evaluation)))))
             # print(str(eval("".join(reversed(evaluation)))))
-            self.ui.labelEnteredExpression.setText("".join(reversed(evaluation)) + " =")
-            self.ui.labelEnter.setText(self.calc_stack[-1])
+            self.ui.labelEnteredExpression.setText(sci_round("".join(reversed(evaluation))) + " =")
+            self.ui.labelEnter.setText(sci_round(self.calc_stack[-1]))
         else:
-            self.ui.labelEnteredExpression.setText(self.calc_stack[-1] + " =")
+            self.ui.labelEnteredExpression.setText(sci_round(self.calc_stack[-1]) + " =")
 
     def realize_clear(self):
         print(self.calc_stack.stack)
@@ -115,7 +122,7 @@ class Window(QtWidgets.QMainWindow):
     def realize_clear_eval(self):
         print(self.calc_stack.stack)
 
-        if (self.calc_stack[-1].isdigit()):
+        if (is_float(self.calc_stack[-1])):
             self.calc_stack[-1] = "0"
         else:
             self.calc_stack.push("0")
@@ -124,11 +131,17 @@ class Window(QtWidgets.QMainWindow):
 
     def realize_del(self):
         print(self.calc_stack.stack)
-        if (self.calc_stack[-1].isdigit()):
+        if (is_float(self.calc_stack[-1])):
             self.calc_stack[-1] = self.calc_stack[-1][0:-1]
             if self.calc_stack[-1] == "":
-                self.calc_stack[-1] = 0
-            self.ui.labelEnter.setText(self.calc_stack[-1])
+                self.calc_stack[-1] = "0"
+            self.ui.labelEnter.setText(sci_round(self.calc_stack[-1]))
+
+    def realize_dot(self):
+        if (is_float(self.calc_stack[-1])):
+            if self.calc_stack[-1].count(".") == 0:
+                self.calc_stack[-1] += "."
+                self.ui.labelEnter.setText(sci_round(self.calc_stack[-1]))
 
 
 if __name__ == "__main__":
